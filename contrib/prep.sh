9deff97a9f65f9c1c7a7acc44d063dfc5827c53f
Scott Trent <trent@jp.ibm.com>
2023-04-05 13:13:53 +0900

    automate xla container setup

---
+++ contrib/prep.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+export GPU_NUM_DEVICES=1
+export FORCE_CUDA=1
+
+pip install https://storage.googleapis.com/tpu-pytorch/wheels/cuda/118/torch_xla-nightly-cp38-cp38-linux_x86_64.whl
+pip install pyyaml
+pip install 'numpy<1.23.0'
+
+
+cd 
+mkdir -p git
+cd git
+
+git clone https://github.com/pytorch/data.git
+cd data
+python setup.py install
+cd -
+
+git clone https://github.com/pytorch/text torchtext
+cd torchtext
+git submodule update --init --recursive
+python setup.py clean install
+cd -
+
+git clone --recursive https://github.com/pytorch/kineto.git
+cd kineto/libkineto
+mkdir -p build
+cd build
+cmake .. && make && make install
+cd 
+cd git
+
+apt-get update
+apt-get install ffmpeg
+pip install ffmpeg
+git clone https://github.com/pytorch/audio
+cd audio
+python setup.py clean install
+cd -
+
+# add some sanity testing
+
+cd benchmarkxla
+python install.py BERT_pytorch vgg16 resnet18

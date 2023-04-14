1eb76245c1eb0b98a917f52b6128eada90a04a9f
Scott Trent <trent@jp.ibm.com>
2023-04-14 13:48:03 +0900

    update

--- contrib/loggedprofilerunbase.sh
+++ contrib/loggedprofilerunbase.sh
@@ -33,7 +33,7 @@ do
 	echo
 	echo Trying simple logged profile ${MODE} run ${TYPE} on model ${model}
 	echo
-       	python ${top}/run.py --vlog -m ${MODE} -d xla -t eval ${model} 2>&1 | tee /data/${TYPE}-${MODE}-${model}-out.txt
+	python ${top}/run.py --vlog -m ${MODE} -d cuda -t eval --profile --profile-detailed ${model} 2>&1 | tee /data/${TYPE}-${MODE}-${model}-out.txt
 	mv logs /data/${TYPE}-${MODE}-${model}-logs
 	echo
 	echo

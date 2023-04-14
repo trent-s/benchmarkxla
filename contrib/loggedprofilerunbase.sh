3570568fd157ff10f7599e307deb5dc4ad2dd94a
Scott Trent <trent@jp.ibm.com>
2023-04-14 13:38:46 +0900

    detailed logging

--- contrib/loggedprofilerunbase.sh
+++ contrib/loggedprofilerunbase.sh
@@ -1,8 +1,28 @@
 #/bin/bash
 
+export TRITON_CACHE_DIR=$top/triton
+export TORCHINDUCTOR_CACHE_DIR=$top/inductor
+export TORCH_COMPILE_DEBUG=1
+
+## Dynamo Env Options
+export TORCHDYNAMO_VERBOSE=1
+export PYTORCH_MATCHER_LOGLEVEL=1
+export TORCHDYNAMO_PRINT_MISSING=1
+export TORCHDYNAMO_PRINT_GUARDS=1
+
+## Inductor Env Options
+export INDUCTOR_WRITE_SCHEDULER_GRAPH=1
+export TORCHINDUCTOR_WRITE_MISSING_OPS=1
+
+## Functorch/AOTAutograd/Other Env Options
+export AOT_FX_GRAPHS=True
+export AOT_PARTITIONER_DEBUG=True
+export PYTORCH_MATCHER_LOGLEVEL=DEBUG
+export PYTORCH_JIT_STATS=True
+
 export GPU_NUM_DEVICES=1
 export FORCE_CUDA=1
-export TYPE=base
+export TYPE=cuda
 
 top=$(dirname "$0")/..
 
@@ -13,9 +33,8 @@ do
 	echo
 	echo Trying simple logged profile ${MODE} run ${TYPE} on model ${model}
 	echo
-        ${top}/contrib/loggedrun.sh ${top}/run.py --vlog -m ${MODE} -d cuda -t eval ${model} 2>&1 | tee /data/${TYPE}-${MODE}-${model}-out.txt
-        mv logs /data/${TYPE}-${MODE}-${model}-logs
-        mv run.log /data/${TYPE}-${MODE}-${model}-run.log
+       	${top}/run.py --vlog -m ${MODE} -d xla -t eval ${model} 2>&1 | tee /data/${TYPE}-${MODE}-${model}-out.txt
+	mv logs /data/${TYPE}-${MODE}-${model}-logs
 	echo
 	echo
     done

f47df05427bd9b453977e6eb8db5217ad4989f1d
Scott Trent <trent@jp.ibm.com>
2023-04-14 11:40:25 +0900

    automate logged profiling

---
+++ contrib/loggedprofilerunbase.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+export GPU_NUM_DEVICES=1
+export FORCE_CUDA=1
+export TYPE=base
+
+top=$(dirname "$0")/..
+
+for MODE in eager jit
+do
+    for model in BERT_pytorch vgg16 resnet18 resnet50 resnext50_32x4d alexnet mobilenet_v2 mnasnet1_0 squeezenet1_1 timm_vision_transformer
+    do
+	echo
+	echo Trying simple logged profile ${MODE} run ${TYPE} on model ${model}
+	echo
+        python ${top}/run.py --vlog -m ${MODE} -d cuda -t eval ${model} 2>&1 | tee /data/${TYPE}-${MODE}-${model}-out.txt
+        mv logs /data/${TYPE}-${MODE}-${model}-logs
+	echo
+	echo
+    done
+done

6143186605162f9a2e79b30a477dfdbf256ef4aa
Scott Trent <trent@jp.ibm.com>
2023-04-17 13:47:30 +0900

    experimentation

--- torchbenchmark/__init__.py
+++ torchbenchmark/__init__.py
@@ -376,6 +376,18 @@ class ModelTask(base_task.TaskBase):
 
     def invoke(self) -> None:
         self.worker.run("""
+            import logging
+            torch._dynamo.config.verbose=True
+            torch._dynamo.config.output_code=True
+            torch._dynamo.config.log_level = logging.DEBUG
+            import torch._functorch.config
+            torch._functorch.config.debug_partitioner=True
+            import torch._inductor.config
+            torch._inductor.config.verbose_progress=True
+            torch._inductor.config.debug=True
+            torch._inductor.config.trace.enabled=True
+            torch._inductor.config.trace.info_log=True
+            torch._inductor.config.trace.graph_diagram=True
             model.invoke()
             maybe_sync()
         """)
